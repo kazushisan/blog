6ecb37cd1383c6788331c6d26e93f8db2bd690a7
Kazushi Konosu <kazushi@konosu.me>
2024-10-07 02:22:52 +0900

    chore: organize config files

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-require-imports */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.bs.js'],
