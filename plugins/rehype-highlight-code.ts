eddfd02954e0e5a1361edd0a65fe15f138b13c2c
Kazushi Konosu <kazushi@konosu.me>
2024-10-09 03:16:36 +0900

    feat: make code plugin type safe

--- plugins/rehype-highlight-code.ts
+++ plugins/rehype-highlight-code.ts
@@ -3,29 +3,33 @@ import { toText } from 'hast-util-to-text';
 import shiki from 'shiki';
 import { removePosition } from 'unist-util-remove-position';
 import rehypeParse from 'rehype-parse';
-import { Plugin, unified } from 'unified';
+import { Transformer, unified } from 'unified';
+import { Element } from 'hast';
 
 const highlighter = await shiki.getHighlighter({ theme: 'nord' });
 const languages = highlighter.getLoadedLanguages();
 
 const parse = unified().use(rehypeParse, { fragment: true }).parse;
 
-const highlightCode: Plugin = () => (tree) => {
-  // fixme
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  visit(tree, { type: 'element', tagName: 'pre' }, (element: any) => {
-    if (element.children[0]?.tagName !== 'code') {
+const highlightCode = (): Transformer<Element> => (tree) => {
+  visit(tree, { type: 'element', tagName: 'pre' }, (element) => {
+    if (
+      !element.children[0] ||
+      !('tagName' in element.children[0]) ||
+      element.children[0].tagName !== 'code'
+    ) {
       return;
     }
 
     const code = toText(element, { whitespace: 'pre' }).trim();
-    const lang = element.children[0].properties.className?.[0]?.startsWith(
-      'language-',
-    )
-      ? element.children[0].properties.className[0].slice(9).toLowerCase()
-      : '';
+    const lang =
+      Array.isArray(element.children[0].properties?.className) &&
+      typeof element.children[0].properties.className[0] == 'string' &&
+      element.children[0].properties?.className?.[0].startsWith('language-')
+        ? element.children[0].properties.className[0].slice(9).toLowerCase()
+        : '';
 
-    if (!languages.includes(lang)) {
+    if (!languages.find((it) => it === lang)) {
       element.properties = {
         style: 'background-color: #2e3440ff; color: #d8dee9;',
       };
@@ -33,9 +37,11 @@ const highlightCode: Plugin = () => (tree) => {
     }
 
     const result = highlighter.codeToHtml(code, { lang });
+    const parsed = removePosition(parse(result)).children[0];
 
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const parsed = removePosition(parse(result)).children[0] as any;
+    if (!parsed || !('properties' in parsed) || !('children' in parsed)) {
+      return;
+    }
 
     element.properties = parsed.properties;
     element.children = parsed.children;
