b368363f72bd8d73016cd937bf75594bd4aaf1e1
Kazushi Konosu <kazushi@konosu.me>
2024-12-24 02:25:04 +0900

    refactor: remove format date logic

--- .vitepress/theme/posts.data.ts
+++ .vitepress/theme/posts.data.ts
@@ -15,23 +15,12 @@ export default createContentLoader('post/*.md', {
       .map(({ url, frontmatter }) => ({
         title: frontmatter.title,
         url,
-        date: formatDate(frontmatter.date),
+        date: frontmatter.date,
       }))
-      .sort((a, b) => b.date.timestamp - a.date.timestamp);
+      .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
   },
 });
 
 declare const data: Post[];
 
 export { data };
-
-const formatDate = (raw: string) => {
-  const date = new Date(raw);
-
-  return {
-    timestamp: date.getTime(),
-    string: date.toLocaleDateString('en-US', {
-      dateStyle: 'long',
-    }),
-  };
-};
