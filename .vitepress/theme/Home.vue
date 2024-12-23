7d80a2e921985f99f41d3380721c4afb1cb5ba64
Kazushi Konosu <kazushi@konosu.me>
2024-12-23 22:52:45 +0900

    fix: fix className to class

--- .vitepress/theme/Home.vue
+++ .vitepress/theme/Home.vue
@@ -3,16 +3,16 @@ import { data as posts } from './posts.data.js';
 </script>
 
 <template>
-  <div className="xl:flex xl:justify-center">
-    <div className="container md:mx-auto xl:mx-0 max-w-4xl flex-1 min-w-0">
-      <div className="px-4">
+  <div class="xl:flex xl:justify-center">
+    <div class="container md:mx-auto xl:mx-0 max-w-4xl flex-1 min-w-0">
+      <div class="px-4">
         <div
           v-for="post in posts"
           :key="post.url"
-          className="mt-8 pt-8 first-of-type:border-t-0 border-t border-slate-100"
+          class="mt-8 pt-8 first-of-type:border-t-0 border-t border-slate-100"
         >
           <a :href="post.url">
-            <h1 className="font-bold text-xl mb-4">{{ post.title }}</h1>
+            <h1 class="font-bold text-xl mb-4">{{ post.title }}</h1>
           </a>
           <!-- todo: add post meta -->
         </div>
--- .vitepress/theme/Post.vue
+++ .vitepress/theme/Post.vue
@@ -4,22 +4,22 @@ import { useData } from 'vitepress';
 const { frontmatter } = useData();
 </script>
 <template>
-  <div className="xl:flex xl:justify-center">
+  <div class="xl:flex xl:justify-center">
     <div
-      className="container md:mx-auto xl:mx-0 max-w-4xl box-content flex-none"
+      class="container md:mx-auto xl:mx-0 max-w-4xl box-content flex-none"
     >
-      <div className="px-4">
+      <div class="px-4">
         <header>
-          <h1 className="font-bold text-3xl my-8">{{ frontmatter.title }}</h1>
-          <div className="my-8">todo: post meta</div>
+          <h1 class="font-bold text-3xl my-8">{{ frontmatter.title }}</h1>
+          <div class="my-8">todo: post meta</div>
         </header>
-        <article className="prose mb-16 max-w-none prose-slate">
+        <article class="prose mb-16 max-w-none prose-slate">
           <Content />
         </article>
       </div>
     </div>
-    <div className="hidden xl:block flex-none w-72">
-      <div className="p-4 sticky mt-40 inset-0 max-h-screen overflow-y-scroll">
+    <div class="hidden xl:block flex-none w-72">
+      <div class="p-4 sticky mt-40 inset-0 max-h-screen overflow-y-scroll">
         todo: table of contents
       </div>
     </div>
