86288b5c8e0389a25475d17c78737ba452ad0fc3
Kazushi Konosu <kazushi@konosu.me>
2024-12-24 03:52:17 +0900

    chore: remove old config files

--- content.config.js
+++
@@ -1,5 +0,0 @@
-// config for vite-plugin-content
-const getTime = (date) => (new Date(date).getTime() / 1000) | 0;
-
-export const query = (list) =>
-  list.sort((a, b) => (getTime(b.data.date) - getTime(a.data.date)) | 0);
