2ea63df805e3ff710d2c449a29e694d6da54f9fa
Kazushi Konosu <kazushi@konosu.me>
2024-12-23 22:54:13 +0900

    chore: remove unused react components

--- src/components/Head.tsx
+++
@@ -1,8 +0,0 @@
-import { Helmet } from 'react-helmet-async';
-
-export const Head = ({ title, path }: { title: string; path: string }) => (
-  <Helmet>
-    <title>{title}</title>
-    <link href={`https://gadgetlunatic.com${path}`} rel="canonical" />
-  </Helmet>
-);
