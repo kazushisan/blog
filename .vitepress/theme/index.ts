d1a77e5aa570f5b8a4dcbfab93ae7fe5a2e1924d
Kazushi Konosu <kazushi@konosu.me>
2024-12-25 02:14:11 +0900

    chore: simplify index

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,11 +1,7 @@
-// https://vitepress.dev/guide/custom-theme
 import Layout from './Layout.vue';
 import type { Theme } from 'vitepress';
 import './style.css';
 
 export default {
   Layout,
-  // enhanceApp({ app, router, siteData }) {
-  //   // ...
-  // }
 } satisfies Theme;
