9a6b8c93d3ce5371eeb238a7d3c9db0c31f8b170
Kazushi Konosu <kazushi@konosu.me>
2024-12-25 02:39:07 +0900

    chore: use separate config for tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,5 @@
 import tailwind from 'tailwindcss';
-import typography from '@tailwindcss/typography';
 
 export default {
-  plugins: [
-    tailwind({
-      content: ['./.vitepress/theme/**/*.vue'],
-      plugins: [typography],
-    }),
-  ],
+  plugins: [tailwind()],
 };
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./.vitepress/theme/**/*.vue'],
+  plugins: [typography],
+};
